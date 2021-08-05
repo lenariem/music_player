21ba9eb45a2f6856ab91045e230167c12c6dacba
unknown <olena.denysova88@gmail.com>
2021-08-05 12:32:45 +0200

    prev and next btns functions

--- script.js
+++ script.js
@@ -53,3 +53,23 @@ playBtn.addEventListener('click', () => {
   }
 });
 
+nextBtn.addEventListener('click', () => {
+  ++songIndex;
+  if(songIndex >= songs.length) {
+    songIndex = 0;
+  }
+  
+  loadSong(songs[songIndex]);
+  playSong();
+});
+
+prevBtn.addEventListener('click', () => {
+  --songIndex;
+  if(songIndex < 0) {
+    songIndex = songs.length-1;
+  }
+  
+  loadSong(songs[songIndex]);
+  playSong();
+});
+
