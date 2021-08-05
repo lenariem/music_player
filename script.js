d1ed62a6c3ed66e6650ec86f6abe8eef110d1127
unknown <olena.denysova88@gmail.com>
2021-08-05 15:21:03 +0200

    progress bar

--- script.js
+++ script.js
@@ -42,7 +42,47 @@ function pauseSong() {
     audio.pause();
 }
 
+// Next song
+function nextSong() {
+  songIndex++;
+  if(songIndex >= songs.length) {
+    songIndex = 0;
+  }
+  
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+//Prev song
+function prevSong() {
+  songIndex--;
+  if(songIndex < 0) {
+    songIndex = songs.length-1;
+  }
+  
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const {duration, currentTime} = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  //total width
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+
 //Event listeners
+
+// Play
 playBtn.addEventListener('click', () => {
   const isPlaying = musicContainer.classList.contains('play');
   
@@ -53,23 +93,16 @@ playBtn.addEventListener('click', () => {
   }
 });
 
-nextBtn.addEventListener('click', () => {
-  ++songIndex;
-  if(songIndex >= songs.length) {
-    songIndex = 0;
-  }
-  
-  loadSong(songs[songIndex]);
-  playSong();
-});
+// Change song
+nextBtn.addEventListener('click', nextSong);
 
-prevBtn.addEventListener('click', () => {
-  --songIndex;
-  if(songIndex < 0) {
-    songIndex = songs.length-1;
-  }
-  
-  loadSong(songs[songIndex]);
-  playSong();
-});
+prevBtn.addEventListener('click', prevSong);
 
+// Progress bar
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+//Song ends
+audio.addEventListener('ended', nextSong);
