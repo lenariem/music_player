f0a5d970db4eb2a300e041ebe3890abdc2c6f5dd
unknown <olena.denysova88@gmail.com>
2021-08-09 17:51:03 +0200

    set volume

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const musicContainer = document.getElementById('music-container');
 const playBtn = document.getElementById('play');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
+const maxVolumeBtn = document.getElementById('max-volume-btn');
+const minVolumeBtn = document.getElementById('min-volume-btn');
 const audio = document.getElementById('audio');
 const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
@@ -9,6 +11,8 @@ const title = document.getElementById('title');
 const cover = document.getElementById('cover');
 const timeFull = document.getElementById('time-full');
 const timeLeft = document.getElementById('time-left');
+const sliderContainer = document.getElementById('slider_container');
+const volumeSlider = document.getElementById('volume_slider');
 
 // Song titles
 const songs = ['hey', 'summer', 'ukulele'];
@@ -29,6 +33,7 @@ function loadSong(song) {
 //Play song
 function playSong() {
   musicContainer.classList.add('play');
+  sliderContainer.classList.add('play');
   playBtn.querySelector('i.fas').classList.remove('fa-play');
   playBtn.querySelector('i.fas').classList.add('fa-pause');
 
@@ -38,6 +43,7 @@ function playSong() {
 //Pause song
 function pauseSong() {
     musicContainer.classList.remove('play');
+    sliderContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.add('fa-play');
     playBtn.querySelector('i.fas').classList.remove('fa-pause');
   
@@ -71,9 +77,6 @@ function updateProgress(e) {
   const {duration, currentTime} = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
   progress.style.width = `${progressPercent}%`;
-  
-  //time
-  //setTime(e);
 }
 
 // Set progress bar
@@ -83,9 +86,6 @@ function setProgress(e) {
   const clickX = e.offsetX;
   const duration = audio.duration;
   audio.currentTime = (clickX / width) * duration;
-  
-  //time
-  //setTime(e);
 }
 
 //set time
@@ -100,7 +100,7 @@ function setTime(e) {
     }
     timeFull.innerText = `${totalMin}:${totalSec}`;
   }
-  
+
   const currentMin = Math.floor(currentTime / 60);
   let currentSec = Math.floor(currentTime % 60);
   if(currentSec < 10){ 
@@ -108,6 +108,22 @@ function setTime(e) {
   }
   timeLeft.innerText = `${currentMin}:${currentSec}`;
 }
+
+
+//set volume
+function setVolume() {
+  // Set the volume according to the percentage of the volume slider set
+  audio.volume = volumeSlider.value / 100;
+  if (audio.volume > 0) {
+    minVolumeBtn.querySelector('.fa').classList.remove('fa-volume-mute');
+    minVolumeBtn.querySelector('.fa').classList.add('fa-volume-down');
+  } else {
+    minVolumeBtn.querySelector('.fa').classList.remove('fa-volume-down');
+    minVolumeBtn.querySelector('.fa').classList.add('fa-volume-mute');
+  }
+ // console.log("volume", audio.volume, volumeSlider.value)
+}
+
 
 //Event listeners
 
@@ -138,3 +154,18 @@ progressContainer.addEventListener('click', setProgress);
 
 //Song ends
 audio.addEventListener('ended', nextSong);
+
+// Set volume
+volumeSlider.addEventListener('change', setVolume);
+
+maxVolumeBtn.addEventListener('click', () => {
+  audio.volume = 1;
+  volumeSlider.value = 100;
+});
+
+minVolumeBtn.addEventListener('click', () => {
+  audio.volume = 0;
+  volumeSlider.value = 1;
+  minVolumeBtn.querySelector('.fa').classList.remove('fa-volume-down');
+  minVolumeBtn.querySelector('.fa').classList.add('fa-volume-mute');
+})
