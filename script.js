9b8e6d63eb435e23c1e9b110f8b3cdba9254bf09
unknown <olena.denysova88@gmail.com>
2021-08-09 13:13:03 +0200

    set time

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const progress = document.getElementById('progress');
 const progressContainer = document.getElementById('progress-container');
 const title = document.getElementById('title');
 const cover = document.getElementById('cover');
+const timeFull = document.getElementById('time-full');
+const timeLeft = document.getElementById('time-left');
 
 // Song titles
 const songs = ['hey', 'summer', 'ukulele'];
@@ -69,6 +71,9 @@ function updateProgress(e) {
   const {duration, currentTime} = e.srcElement;
   const progressPercent = (currentTime / duration) * 100;
   progress.style.width = `${progressPercent}%`;
+  
+  //time
+  //setTime(e);
 }
 
 // Set progress bar
@@ -78,6 +83,30 @@ function setProgress(e) {
   const clickX = e.offsetX;
   const duration = audio.duration;
   audio.currentTime = (clickX / width) * duration;
+  
+  //time
+  //setTime(e);
+}
+
+//set time
+function setTime(e) {
+  const {duration, currentTime} = e.srcElement;
+  if (!isNaN(duration)) {
+    const totalMin = Math.floor(duration / 60);
+    let totalSec = Math.floor(duration % 60);
+    //if sec is less than 10 then add 0 before it
+    if(totalSec < 10){ 
+      totalSec = `0${totalSec}`;
+    }
+    timeFull.innerText = `${totalMin}:${totalSec}`;
+  }
+  
+  const currentMin = Math.floor(currentTime / 60);
+  let currentSec = Math.floor(currentTime % 60);
+  if(currentSec < 10){ 
+    currentSec = `0${currentSec}`;
+  }
+  timeLeft.innerText = `${currentMin}:${currentSec}`;
 }
 
 //Event listeners
@@ -100,6 +129,9 @@ prevBtn.addEventListener('click', prevSong);
 
 // Progress bar
 audio.addEventListener('timeupdate', updateProgress);
+
+//Set time
+audio.addEventListener('timeupdate', setTime);
 
 // Click on progress bar
 progressContainer.addEventListener('click', setProgress);
